b1b4aca8ee6119c5d8b2c4237f52c454037e5d47
qzore-test <dsafsgfjhb121123af@mail.ru>
2024-01-06 21:56:54 +0300

    Update gui.ts

--- src/gui.ts
+++ src/gui.ts
@@ -114,7 +114,7 @@ export class GUIPlayer {
 		position.Height -= Math.round(position.Height / 1.75)
 
 		const text = `${this.player.LastHitCount}/${this.player.DenyCount}`
-		RendererSDK.TextByFlags(text, position, undefined, 1.5, undefined, 500)
+		RendererSDK.TextByFlags(text, position, undefined, 1.5, undefined, 700)
 	}
 
 	public RenderMana(menu: BarsMenu) {
